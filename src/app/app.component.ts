df49cc51dbc5ea4758de35538c78f03ae6e6bb56
JannickPepe <jannickpedersen@live.dk>
2022-02-14 13:56:51 +0100

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  title = 'Kyle Pedersen';
+  cardContent = 'This is very usefull indeed';
+  url="https://www.w3schools.com?name=ADAM";
   constructor() {}
-
-  
 }
